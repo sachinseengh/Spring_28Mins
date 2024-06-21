9f2b692e12f17a1a01b3064611f5f19055608cae
sachinseengh <sachinseengh@gmail.com>
2024-06-21 19:14:34 +0545

    Check

---
+++ Spring_in28min/src/main/java/com/example/Spring_in28min/SpringIn28minApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_in28min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn28minApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn28minApplication.class, args);
+	}
+
+}
---
+++ Spring_in28min/src/test/java/com/example/Spring_in28min/SpringIn28minApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_in28min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIn28minApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
