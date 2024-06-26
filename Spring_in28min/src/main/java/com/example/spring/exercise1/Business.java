bd7244daf76f24402d1d43883f2a07f2f0bf9bd8
sachinseengh <sachinseengh@gmail.com>
2024-06-26 08:00:02 +0545

    Corrected

--- Spring_in28min/src/main/java/com/example/spring/exercise1/Business.java
+++ Spring_in28min/src/main/java/com/example/spring/exercise1/Business.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
 
+@Configuration
+@ComponentScan
 public class Business {
 
 	public static void main(String[] args) {
 	
 		
 		var context=
-				new AnnotationConfigApplicationContext(BusinessCalculationService.class);
+				new AnnotationConfigApplicationContext(Business.class);
 		
 		
 		//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out:: println);
--- Spring_in28min/src/main/java/com/example/spring/exercise1/BusinessCalculationService.java
+++ Spring_in28min/src/main/java/com/example/spring/exercise1/BusinessCalculationService.java
@@ -6,8 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Configuration
-@ComponentScan
+@Component
 public class BusinessCalculationService {
 	
 	DataService dataService;
